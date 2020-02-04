d1ea5b1ba1bbd2667bdfbf8d140e16974f79d5a2
Connor Klopfer <Connor.Klopfer@uvm.edu>
2020-02-04 11:09:53 -0500

    Updates for running in google colab

--- county_plot.py
+++ county_plot.py
@@ -3,63 +3,34 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 from flask_caching import Cache
+from ipywidgets import widgets
 
 from network_files.graph_building import build_plotly_graph
 from network_files.assemble_network import final_graph, pos, analysis, coloring_opts
 
+
 TIMEOUT = 60
 
-x_pos_node, y_pos_node, x_edges, y_edges, node_labels, node_coloring = build_plotly_graph(final_graph, coloring_opts, analysis, pos)
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# print([{'label': i, 'value': i} for i in coloring_opts])
 
+x_pos_node, y_pos_node, x_edges, y_edges, node_labels, node_coloring = build_plotly_graph(final_graph, \
+    coloring_opts, analysis, pos)
 
-app = dash.Dash(external_stylesheets = external_stylesheets)
-
-cache = Cache(app.server, config = {
-    'CACHE_TYPE': 'filesystem', 
-    'CACHE_DIR' : 'cache'
-})
-
-
-app.layout = html.Div([
-        html.Div([
-            dcc.Graph(
-        id='county_network',
-        style={
-            'width': '99%',
-            'position': 'absolute'}
-        ),  
-        html.Div([
-            dcc.Dropdown(
-                id='rank_choice',
-                options=[{'label': i, 'value': i} for i in coloring_opts],
-                value=coloring_opts[0]
-            ), 
-            dcc.RadioItems(
-                options = [
-                    {'label': 'Heterogeneity', 'value': 'abs_dist'},
-                    {'label': 'Difference', 'value': 'raw_dist'}                    
-                ],
-                id='calc_choice',
-                # options=[{'label': i, 'value': i} for i in coloring_opts],
-                value='abs_dist'
-            )], 
-        style = {
-            'display' : 'inline-block',
-            'position' : 'relative',
-            'top' : 0, 
-            'left': 0, 
-            'width': '20%'
-        })],
-        )])
-
-
-@app.callback(
-    dash.dependencies.Output('county_network', 'figure'),
-    [dash.dependencies.Input('calc_choice', 'value'), 
-    dash.dependencies.Input('rank_choice', 'value')])
-@cache.memoize(timeout = TIMEOUT)
-def update_plotly_graph(analysis_opt, coloring_opt):
+metric_opts = widgets.Dropdown(
+        description='rank_choice',
+        options=[i for i in coloring_opts],
+        value=coloring_opts[0]
+    ) 
+analysis_opts = widgets.RadioButtons(
+        options = ['abs_dist', 'raw_dist'],
+        #     {'label': 'Heterogeneity', 'value': 'abs_dist'},
+        #     {'label': 'Difference', 'value': 'raw_dist'}                    
+        # '],
+        description ='Calculation',
+        # options=[{'label': i, 'value': i} for i in coloring_opts],
+        value='abs_dist'
+    ) 
+def colab_widget(analysis_opt, coloring_opt, x_pos_node, y_pos_node, x_edges, y_edges, node_labels, node_coloring):
     edge_trace = go.Scatter(x=x_edges,
             y=y_edges,
             mode='lines',
@@ -78,7 +49,7 @@ def update_plotly_graph(analysis_opt, coloring_opt):
                 name='actors',
                 marker=dict(symbol='circle',
                                 size=3,
-                                color=node_coloring[analysis_opt][coloring_opt],
+                                color=node_coloring[analysis_opt[0]][coloring_opt[0]],
                                 colorscale='Bluered_r',
                                 line=dict(color='rgb(50,50,50)', width=0.5)
                                 ),
@@ -120,3 +91,22 @@ def update_plotly_graph(analysis_opt, coloring_opt):
         hovermode='closest'#,
     )
     return {'data' : [node_trace, edge_trace], 'layout' : layout}
+
+plotly_graph = colab_widget(analysis, coloring_opts, x_pos_node, y_pos_node, \
+    x_edges, y_edges, node_labels, node_coloring)
+
+option_box = widgets.HBox(children = [analysis_opts, metric_opts])
+
+network = go.FigureWidget(data = plotly_graph['data'], layout = plotly_graph['layout'])
+
+
+def response(change):
+
+    network['data'][0]['marker']['color'] = \
+        node_coloring[analysis_opts.value][metric_opts.value]
+
+analysis_opts.observe(response, names = 'value')
+metric_opts.observe(response, names = 'value')
+
+
+final = widgets.VBox(children = [option_box,network])
